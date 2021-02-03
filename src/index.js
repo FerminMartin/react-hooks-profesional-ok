73c897015f036ed9f09e8b1873a731a47a76703e
fermin <fermartorr01@gmail.com>
2021-02-03 19:30:29 +0100

    3.3 Instalación de webpack y babel: presets, plugins y loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-impor App from './components/App'
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
